8277c74d418d905f72b577e04508bfdea96843bf
nox map <hw19196@gmail.com>
2025-07-29 03:31:35 +0545

    [date_fixing]add: merged all data and stored it

--- data_preprocessing/date_fixing.py
+++ data_preprocessing/date_fixing.py
@@ -1,3 +1,5 @@
+import os
+
 import pandas as pd
 import re
 from pathlib import Path
@@ -9,10 +11,15 @@ script_dir = Path(__file__).resolve().parent
 # Move up one level to the project root
 project_root = script_dir.parent
 
+clean_dir = project_root / "data"
 # Build correct path
-los_path = project_root / "data" / "raw" / "jobs_raw_zip_losangeles.csv"
-new_york_path = project_root / "data" / "raw" / "jobs_raw_zip_newyork.csv"
-remote_path = project_root / "data" / "raw" / "jobs_raw_zip_remote.csv"
+los_path = clean_dir / "raw" / "jobs_raw_zip_losangeles.csv"
+new_york_path = clean_dir / "raw" / "jobs_raw_zip_newyork.csv"
+remote_path = clean_dir / "raw" / "jobs_raw_zip_remote.csv"
+
+#path to store the cleaned and merged data
+all_output_path = clean_dir / "date_cleaned" / "fix_date_jobs.csv"
+os.makedirs(os.path.dirname(all_output_path),exist_ok=True)
 
 print("Resolved path:", los_path)
 
@@ -30,25 +37,35 @@ def extract_days_ago(post_text):
         return float(SCRAPE_DELAY_DAYS)
     return None
 
-def load_and_clean(path):
+def load_and_clean(path,source_name):
     df = pd.read_csv(path)
     df['days_ago'] = df['post_date'].apply(extract_days_ago)
+    df["source"] = source_name
     return df
 
-los_df = load_and_clean(los_path)
-new_york_df = load_and_clean(new_york_path)
-remote_df = load_and_clean(remote_path)
+los_df = load_and_clean(los_path,"los_angleles")
+new_york_df = load_and_clean(new_york_path,"new_york")
+remote_df = load_and_clean(remote_path,"remote")
 
 #remove the old post_date
 los_df = los_df.drop(columns=['post_date'])
 new_york_df = new_york_df.drop(columns=['post_date'])
 remote_df = remote_df.drop(columns=['post_date'])
 
+all_job_df = pd.concat([los_df,new_york_df,remote_df],ignore_index=True)
+
+print("---ALL---")
+print(all_job_df)
+print("=========")
 
 
+all_job_df.to_csv(all_output_path,index = False)
+print(f"Stored {len(all_job_df)}jobs in path : {all_output_path}")
+
+"""
 print("los dataframe")
 print(los_df.head(10))
 print("new_york dataframe")
 print(new_york_df.head(10))
 print("remote dataframe")
-print(remote_df.head(10))
+print(remote_df.head(10))"""
